da562789c93bd3af962be7ab988ad33ec24d1050
Dan Rodriguez <derodriguez20@ucatolica.edu.co>
2025-08-31 15:35:45 -0500

    Ejercicio pasar de morse a texto

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -244,6 +244,40 @@ class Conversion:
         pass
     
     def morse_a_texto(self, morse):
+        def morse_text(morse):
+            morse_dic = {
+                '.-': 'A',    '-...': 'B',
+                '-.-.': 'C',  '-..': 'D',
+                '.': 'E',     '..-.': 'F',
+                '--.': 'G',   '....': 'H',
+                '..': 'I',    '.---': 'J',
+                '-.-': 'K',   '.-..': 'L',
+                '--': 'M',    '-.': 'N',
+                '---': 'O',   '.--.': 'P',
+                '--.-': 'Q',  '.-.': 'R',
+                '...': 'S',   '-': 'T',
+                '..-': 'U',   '...-': 'V',
+                '.--': 'W',   '-..-': 'X',
+                '-.--': 'Y',  '--..': 'Z',
+                '-----': '0',
+                '.----': '1',
+                '..---': '2',
+                '...--': '3',
+                '....-': '4',
+                '.....': '5',
+                '-....': '6',
+                '--...': '7',
+                '---..': '8',
+                '----.': '9',
+                '/': ' '
+            }
+            palabras = morse.split(' / ')
+            resultado = []
+            for palabra in palabras:
+                letras = palabra.split()
+                palabra_decodificada = ''.join(morse_dic.get(letra, '') for letra in letras)
+                resultado.append(palabra_decodificada)
+            return ' '.join(resultado)
         """
         Convierte código Morse a texto.
         
