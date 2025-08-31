80dd6384e744ce9343d4cd021605abc13ac270e5
Dan Rodriguez <derodriguez20@ucatolica.edu.co>
2025-08-31 18:22:55 -0500

    Listas combinadas

--- src/data/data.py
+++ src/data/data.py
@@ -69,7 +69,24 @@ class Data:
         Returns:
             list: Lista combinada y ordenada
         """
-        pass
+        resultado = []
+        i, j = 0, 0
+        while i < len(lista1) and j < len(lista2):
+            if lista1[i] < lista2[j]:
+                resultado.append(lista1[i])
+                i += 1
+            else:
+                resultado.append(lista2[j])
+                j += 1
+        while i < len(lista1):
+            resultado.append(lista1[i])
+            i += 1
+        while j < len(lista2):
+            resultado.append(lista2[j])
+            j += 1
+        return resultado
+    
+    pass
     
     def rotar_lista(self, lista, k):
         """
